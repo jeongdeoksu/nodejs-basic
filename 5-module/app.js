f2d906658c387ec77d3ee33ee2b96acb82108e2c
Deoksu Jeong <jeongdeoksu@gmail.com>
2025-06-29 16:51:32 +0900

    5-module

---
+++ 5-module/app.js
@@ -0,0 +1,14 @@
+// import {getCount, increase} from "./counter.js"
+import * as counter from  './counter.js';
+
+// getCount();
+// increase()
+// increase();
+// increase();
+// console.log(getCount())
+
+counter.getCount();
+counter.increase();
+counter.increase();
+counter.increase();
+console.log(counter.getCount())
